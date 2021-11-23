c3ea40ffea2c20e2a0be8d2c50425c48b0e410c8
hdk3136 <81366376+hdk3136@users.noreply.github.com>
2021-11-23 22:14:50 +0800

    finish the lab3

---
+++ lab3/Flik/FilkTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class FilkTest {
+
+    @Test
+    public void testFilk() {
+        assertEquals(true, Flik.isSameNumber(128,128));
+    }
+}
