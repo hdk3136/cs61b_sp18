b3f95c5776f5fa62855aea67e3ce886d4a2c93b3
hdk3136 <81366376+hdk3136@users.noreply.github.com>
2022-03-09 16:34:27 +0800

    finish the hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -2,6 +2,99 @@ package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+
 public class Percolation {
+    private final int n;
+    private final boolean[][] sites;
+    private WeightedQuickUnionUF wqu;
+    private int numOfOpenSites;
+
+    // create N-by-N grid, with all sites initially blocked
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        this.n = N;
+        sites = new boolean[N][N];
+        for (int i = 0; i < N; i += 1) {
+            for (int j = 0; j < N; j += 1) {
+                sites[i][j] = false;
+            }
+        }
+        wqu = new WeightedQuickUnionUF(N * N);
+        numOfOpenSites = 0;
+    }
+
+    //row is x and col is y
+    private int xyToNum(int x, int y) {
+        return n * x + y;
+    }
+
+    private boolean validate(int row, int col) {
+        return (row >= 0 && row <= n - 1) && (col >= 0 && col <= n - 1);
+    }
+
+    private void updateUnion(int row, int col) {
+        if (validate(row + 1, col) && isOpen(row + 1, col)) {
+            wqu.union(xyToNum(row, col), xyToNum(row + 1, col));
+        }
+        if (validate(row - 1, col) && isOpen(row - 1, col)) {
+            wqu.union(xyToNum(row, col), xyToNum(row - 1, col));
+        }
+        if (validate(row, col + 1) && isOpen(row, col + 1)) {
+            wqu.union(xyToNum(row, col), xyToNum(row, col + 1));
+        }
+        if (validate(row, col - 1) && isOpen(row, col - 1)) {
+            wqu.union(xyToNum(row, col), xyToNum(row, col - 1));
+        }
+    }
+
+    // open the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        if (!validate(row, col)) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+        sites[row][col] = true;
+        numOfOpenSites += 1;
+        updateUnion(row, col);
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        if (!validate(row, col)) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+        return sites[row][col];
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        if (!validate(row, col)) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+        for (int i = 0; i < n; i += 1) {
+            if (wqu.connected(xyToNum(0, i), xyToNum(row, col))) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    // number of open sites
+    public int numberOfOpenSites() {
+        return numOfOpenSites;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        for (int i = 0; i < n; i += 1) {
+            if (isFull(n - 1, i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 
 }
---
+++ hw2/hw2/TestUnit.java
@@ -0,0 +1,16 @@
+package hw2;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestUnit {
+    @Test
+    public void testPercolation() {
+        Percolation test = new Percolation(10);
+        assertFalse(test.isOpen(0, 5));
+        test.open(0, 5);
+        test.open(1, 5);
+        assertTrue(test.isFull(1, 5));
+
+    }
+}
